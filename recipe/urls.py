d048bb7bbd25d9fdf2dd7276d01f4124d02a510b
MykhailynaMarta <marta.mykhailyna@knu.ua>
2025-05-23 12:52:10 +0300

    added function recipe_detail

--- recipe/urls.py
+++ recipe/urls.py
@@ -1,8 +1,9 @@
-from recipe.views import main
+from recipe.views import main, recipe_detail
 from django.urls import path, include
 
 app_main = 'recipe_app'
 
 urlpatterns = [
     path('', main, name='main'),
+    path('recipe_detail/<int:recipe_id>/', recipe_detail, name='recipe_detail'),
 ]
--- recipe/views.py
+++ recipe/views.py
@@ -7,3 +7,8 @@ def main(request):
     recipes = Recipe.objects.filter(created_at__year=2023)
 
     return render(request, 'main.html', context={'recipes': recipes})
+
+def recipe_detail(request, recipe_id):
+    recipes = Recipe.objects.get(id=recipe_id)
+
+    return render(request, 'recipe_detail.html', context={'recipes': recipes})
